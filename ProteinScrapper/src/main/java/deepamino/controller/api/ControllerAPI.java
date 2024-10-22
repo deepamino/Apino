774ea9b5560aaa56605b4086c04f90d35a447382
Ricardo Cárdenes <78762064+ricardocardn@users.noreply.github.com>
2024-10-22 12:49:40 +0100

    Code Refactor

--- ProteinScrapper/src/main/java/deepamino/controller/api/ControllerAPI.java
+++ ProteinScrapper/src/main/java/deepamino/controller/api/ControllerAPI.java
@@ -4,7 +4,7 @@ import deepamino.controller.fasta.FastaHandler;
 
 public record ControllerAPI(FastaHandler fastaHandler) {
     public void run() {
-        ProteinAPI proteinAPI = new ProteinAPI(fastaHandler);
-        proteinAPI.run();
+        NucleotideAPI nucleotideAPI = new NucleotideAPI(fastaHandler);
+        nucleotideAPI.run();
     }
 }
--- ProteinScrapper/src/main/java/deepamino/controller/api/NucleotideAPI.java
+++ ProteinScrapper/src/main/java/deepamino/controller/api/NucleotideAPI.java
@@ -3,23 +3,14 @@ package deepamino.controller.api;
 import com.google.gson.Gson;
 import deepamino.controller.fasta.FastaHandler;
 import deepamino.controller.fetcher.NCBISequenceFetcher;
-import deepamino.model.fasta.RawFastaFile;
 
 import static spark.Spark.*;
 
-public record ProteinAPI(FastaHandler fastaHandler) {
+public record NucleotideAPI(FastaHandler fastaHandler) {
 
     public void run() {
         port(8080);
         getInfo();
-    }
-
-    public void postFasta() {
-        post("/api/resource/fasta", (req, res) -> {
-            RawFastaFile fasta = (new Gson()).fromJson(req.body(), RawFastaFile.class);
-            res.status(201);
-            return new Gson().toJson(fasta);
-        });
     }
 
     public void getInfo() {
@@ -30,6 +21,4 @@ public record ProteinAPI(FastaHandler fastaHandler) {
             );
         });
     }
-
-    public void getProtein() {}
 }
