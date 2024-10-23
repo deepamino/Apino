19ee2f78c6834bf009f919c3bc1b329d32bf0699
Ricardo Cárdenes <78762064+ricardocardn@users.noreply.github.com>
2024-10-23 18:55:34 +0100

    Code Refactor

--- Apino/src/main/java/deepamino/Main.java
+++ Apino/src/main/java/deepamino/Main.java
@@ -1,20 +1,12 @@
 package deepamino;
 
 import deepamino.controller.Controller;
-import deepamino.controller.fetcher.SequenceNCBIFetcher;
-import deepamino.controller.scrapper.GeneScrapper;
-import deepamino.model.fasta.ProcessedFastaFile;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         new Controller().run();
-        Map<String, String> map = new HashMap<>();
-        map.put("NM_001301717", "");
-        System.out.println(new SequenceNCBIFetcher().fetch("NM_001301717"));
     }
 }
 
